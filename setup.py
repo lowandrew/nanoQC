e614d6d3fe9b62fd91e0d9e40075fe580d63cf47
Andrew Low <andrew.low@canada.ca>
2018-10-19 15:59:22 -0400

    update setup

--- setup.py
+++ setup.py
@@ -6,8 +6,7 @@ setup(
     name='nanoqc',
     version='0.0.1',
     packages=find_packages(),
-    scripts=['nanoqc/nanoQC.py',
-             'nanoqc/faster_nanoQC.py'],
+    scripts=['nanoqc/nanoQC.py']
     author='Marc-Olivier Duceppe',
     author_email="andrew.low@canada.ca",
     url='https://github.com/lowandrew/nanoQC',
